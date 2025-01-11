d75fc5d717eae66166645f0d6d0fb7cff2e005c7
gleichmut <your_felicia@mail.ru>
2025-01-11 17:52:04 +0300

    Переписан тест RegistrationViewModel

--- app/src/androidTest/java/com/fuxkinghatred/diaryofemotions/RegistrationViewModelTest.java
+++ app/src/androidTest/java/com/fuxkinghatred/diaryofemotions/RegistrationViewModelTest.java
@@ -7,8 +7,8 @@ import androidx.lifecycle.Observer;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.fuxkinghatred.diaryofemotions.viewmodels.RegistrationViewModel;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -17,8 +17,6 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -31,16 +29,12 @@ public class RegistrationViewModelTest {
     @Mock
     private FirebaseUser mockFirebaseUser;
     @Mock
-    private Task<AuthResult> mockAuthTask;
-    @Mock
     private Task<Void> mockVoidTask;
     @Mock
     private Observer<String> errorObserver;
     @Mock
     private Observer<FirebaseUser> userObserver;
-    @Captor
-    private ArgumentCaptor<OnSuccessListener<AuthResult>> authResultCaptor;
-    @Captor
+
     private RegistrationViewModel viewModel;
     private static final String TEST_EMAIL = "test@test.ru";
     private static final String TEST_PASSWORD = "password";
@@ -54,19 +48,16 @@ public class RegistrationViewModelTest {
         viewModel.getError().observeForever(errorObserver);
         viewModel.getUser().observeForever(userObserver);
         when(mockAuth.getCurrentUser()).thenReturn(mockFirebaseUser);
-        when(mockAuth.createUserWithEmailAndPassword(anyString(), anyString())).thenReturn(mockAuthTask);
+        when(mockAuth.createUserWithEmailAndPassword(anyString(), anyString()))
+                .thenReturn(Tasks.forResult(mock(AuthResult.class)));
         when(mockFirebaseUser.updateProfile(any())).thenReturn(mockVoidTask);
     }
 
     @Test
     public void register_userNotCreated() {
-        when(mockAuthTask.isSuccessful()).thenReturn(true);
-        when(mockAuthTask.getResult()).thenReturn(mock(AuthResult.class));
         when(mockAuth.getCurrentUser()).thenReturn(null);
         viewModel.register(TEST_EMAIL, TEST_PASSWORD, TEST_NAME);
         verify(mockAuth).createUserWithEmailAndPassword(TEST_EMAIL, TEST_PASSWORD);
-        verify(mockAuthTask).addOnSuccessListener(authResultCaptor.capture());
-        authResultCaptor.getValue().onSuccess(mock(AuthResult.class));
         verify(mockFirebaseUser, never()).updateProfile(any());
         verify(userObserver, never()).onChanged(mockFirebaseUser);
     }
